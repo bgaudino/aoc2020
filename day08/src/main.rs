e5ea3968ac768f5067a78d75d877b85b5093bdeb
bgaudino <bgaudino@gmail.com>
2024-01-26 13:51:25 -0600

    Day 8 part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,20 +1,17 @@
 use std::collections::HashSet;
 use std::fs;
 
-fn main() {
-    let contents = fs::read_to_string("data.txt").unwrap();
-    let mut instructions: Vec<(&str, i32)> = Vec::new();
-    for line in contents.lines() {
-        let parts: Vec<&str> = line.split(" ").collect();
-        let instruction = parts[0];
-        let n = parts[1].parse::<i32>().unwrap();
-        instructions.push((instruction, n));
-    }
+fn run(instructions: &Vec<(&str, i32)>) -> (i32, bool) {
     let mut i: i32 = 0;
     let mut accum: i32 = 0;
     let mut visited: HashSet<i32> = HashSet::new();
+    let mut terminates = false;
     loop {
         if visited.contains(&i) {
+            break;
+        }
+        if i as usize >= instructions.len() {
+            terminates = true;
             break;
         }
         visited.insert(i);
@@ -33,5 +30,33 @@ fn main() {
             _ => panic!("Invalid instruction"),
         };
     }
-    println!("Part 1: {}", accum);
+    (accum, terminates)
+}
+
+fn main() {
+    let contents = fs::read_to_string("data.txt").unwrap();
+    let mut instructions: Vec<(&str, i32)> = Vec::new();
+    for line in contents.lines() {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let instruction = parts[0];
+        let n = parts[1].parse::<i32>().unwrap();
+        instructions.push((instruction, n));
+    }
+    let (part1, _) = run(&instructions);
+    println!("Part 1: {}", part1);
+
+    for i in 0..instructions.len() {
+        let (instruction, n) = instructions[i];
+        if instruction == "nop" || instruction == "jmp" {
+            let new_instruction = if instruction == "nop" { "jmp" } else { "nop" };
+            instructions[i] = (new_instruction, n);
+            let (accum, terminates) = run(&instructions);
+            if terminates {
+                println!("Part 2: {}", accum);
+                break;
+            } else {
+                instructions[i] = (instruction, n);
+            }
+        }
+    }
 }
