08cad918f12e14e1bffadd410ac09d386d551f4e
bgaudino <bgaudino@gmail.com>
2024-02-07 10:45:17 -0600

    Day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use num::integer::lcm;
+use std::{f32::INFINITY, fs};
 
 struct Bus(usize);
 
@@ -6,32 +7,61 @@ impl Bus {
     fn wait_time(&self, timestamp: usize) -> usize {
         (timestamp / self.0 + 1) * self.0 - timestamp
     }
+
+    fn has_departure_at(&self, timestamp: usize) -> bool {
+        timestamp % self.0 == 0
+    }
 }
 
 fn main() {
     let contents = fs::read_to_string("data.txt").unwrap();
     let (buses, timestamp) = parse(contents);
-    let mut bus_id = buses[0].0;
-    let mut min_wait_time = buses[0].wait_time(timestamp);
-    for bus in &buses[1..] {
-        let wait_time = bus.wait_time(timestamp);
-        if wait_time < min_wait_time {
-            min_wait_time = wait_time;
-            bus_id = bus.0;
-        } 
+    let mut bus_id = 0;
+    let mut min_wait_time = INFINITY as usize;
+    for bus in &buses {
+        if let Some(b) = bus {
+            let wait_time = b.wait_time(timestamp);
+            if wait_time < min_wait_time {
+                min_wait_time = wait_time;
+                bus_id = b.0;
+            }
+        }
     }
     let part1 = bus_id * min_wait_time;
+    assert_eq!(part1, 4938);
     println!("Part 1: {}", part1);
+
+    let mut interval: usize = 1;
+    let mut part2: usize = 0;
+    loop {
+        let mut is_target = true;
+        for (j, bus) in buses.iter().enumerate() {
+            if let Some(b) = bus {
+                if !b.has_departure_at(part2 + j) {
+                    is_target = false;
+                    break;
+                } else {
+                    interval = lcm(interval, b.0);
+                }
+            }
+        }
+        if is_target {
+            break;
+        }
+        part2 += interval;
+    }
+    assert_eq!(part2, 230903629977901);
+    println!("Part 2: {}", part2);
 }
 
-fn parse(s: String) -> (Vec<Bus>, usize) {
+fn parse(s: String) -> (Vec<Option<Bus>>, usize) {
     let parts: Vec<&str> = s.lines().collect();
     let timestamp: usize = parts[0].parse().unwrap();
-    let mut buses: Vec<Bus> = vec![];
+    let mut buses: Vec<Option<Bus>> = vec![];
     for part in parts[1].split(",") {
         match part.parse::<usize>() {
-            Ok(v) => buses.push(Bus(v)),
-            _ => (),
+            Ok(v) => buses.push(Some(Bus(v))),
+            _ => buses.push(None),
         }
     }
     (buses, timestamp)
